081b9deb1b36a740cbc04cc822eb05daedf92282
franri <franrizzo.98@gmail.com>
2019-10-21 17:54:14 -0300

    Tabs alineadas a la derecha, y se juntan sólo en s screen

--- js/feed.js
+++ js/feed.js
@@ -1,8 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     var elems = document.querySelectorAll('.tabs');
-    console.log(elems);
-    var options = {swipeable: true, responsiveThreshold : 1920};
+    var options = {swipeable: true};
     var instance = M.Tabs.init(elems[1], options);
 
 
