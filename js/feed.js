4ea662975b773c55c3aa20dd6595e36ace5fb529
Juan José Regent <jregent@correo.um.edu.uy>
2019-11-03 18:28:45 -0300

    tweets Feed

--- js/feed.js
+++ js/feed.js
@@ -4,10 +4,17 @@ document.addEventListener('DOMContentLoaded', function () {
     var optionsTabs = {swipeable: true};
     var instanceTabs = M.Tabs.init(elemsTabs[1], optionsTabs);
     
-     var elemsCar = document.querySelectorAll('.carousel');
-     var optionsCar = {duration: 200, swipeable: true, dist: 0, padding: 0, fullWidth: false, indicators: true, numVisible: 5}; 
-     var instancesCar = M.Carousel.init(elemsCar[0], optionsCar);
+    var elemsCar = document.querySelectorAll('.carousel');
+    var optionsCar = {duration: 200, swipeable: true, dist: 0, padding: 0, fullWidth: false, indicators: true, numVisible: 5}; 
+    var instancesCar = M.Carousel.init(elemsCar[0], optionsCar);
     
+    var elemsMedia = document.querySelectorAll('.materialboxed');
+    var optionsMedia = {inDuration: 400};
+    var instancesMedia = M.Materialbox.init(elemsMedia, optionsMedia);
+
+    var elemsCollapsible = document.querySelectorAll('.collapsible.expandable');
+    var optionsCollapsible= {accordion: false};
+    var instancesCollapsible = M.Collapsible.init(elemsCollapsible, optionsCollapsible);
 
 });
 
