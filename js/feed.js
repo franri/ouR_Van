5d70c316dd14628e9a0c4c80e2069720e3be2346
franri <franrizzo.98@gmail.com>
2019-11-09 17:19:19 -0300

    commit to pull

--- js/feed.js
+++ js/feed.js
@@ -16,6 +16,26 @@ document.addEventListener('DOMContentLoaded', function () {
     var optionsCollapsible= {accordion: false};
     var instancesCollapsible = M.Collapsible.init(elemsCollapsible, optionsCollapsible);
 
+    var b_satisfecho = document.querySelector("#boton-satisfecho");
+    var b_neutral = document.querySelector("#boton-neutral");
+    var b_insatisfecho = document.querySelector("#boton-insatisfecho");
+
+    var c_satisfecho = document.querySelector("#contador-satisfecho");
+    var c_neutral = document.querySelector("#contador-neutral");
+    var c_insatisfecho = document.querySelector("#contador-insatisfecho");
+
+    b_satisfecho.addEventListener( 'click', function() {
+        c_satisfecho.innerHTML = parseInt(c_satisfecho.innerHTML) + 1;
+    });
+    b_neutral.addEventListener( 'click', function() {
+        c_neutral.innerHTML = parseInt(c_neutral.innerHTML) + 1;
+    });
+    b_insatisfecho.addEventListener( 'click', function() {
+        c_insatisfecho.innerHTML = parseInt(c_insatisfecho.innerHTML) + 1;
+    });
+
+
+
 });
 
 window.onresize = function(){location.reload();};
