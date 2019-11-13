6b6776467130b507a2dce5fac48179dc3e24fa19
Juan José Regent <jregent@correo.um.edu.uy>
2019-11-13 16:10:42 -0300

    contacto

--- js/contacto.js
+++ js/contacto.js
@@ -1,3 +1,37 @@
-document.addEventListener('DOMContentLoaded', function() {
+// document.addEventListener('DOMContentLoaded', function () {
+  
+//   document.querySelector("#send-button").addEventListener("click",function(){
+//     let nombre = document.getElementById("first_name").value;
+//     let apellido = document.getElementById("last_name").value;
+//     let email = document.getElementById("email").value;
+//     let text = document.getElementById("textarea1").value;
+//     let checked_value = null;
+  
+//     if (document.getElementById("employment").checked) {
+//       checked_value = "employment";
+//     } else if ((document.getElementById("rental").checked)) {
+//       checked_value = "rental";
+//     } else if ((document.getElementById("business").checked)) {
+//       checked_value = "business";
+//     }
+  
+//     Email.send({
+//       Host : "smtp.yandex.com",
+//       Username : "ChapitaTic3",
+//       Password : "SomosChapita",
+//       To : 'juanregent@gmail.com',
+//       From : "ChapitaTic3@yandex.com",
+//       Subject : "Formulario de Chapita",
+//       Body : `Nombre: ${nombre}
+//               Apellido: ${apellido}`
+//   }).then(
+//     message => alert(message)
+//   );
 
-  });
+
+
+
+//   });
+  
+
+// });
