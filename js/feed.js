091f2d323621146da79279b56f68f3c37b5ab5c4
franri <franrizzo.98@gmail.com>
2019-11-15 02:07:21 -0300

    Entrega final

--- js/feed.js
+++ js/feed.js
@@ -39,7 +39,7 @@ let data = [{
             {
                 image: "https://picsum.photos/id/7/500/",
                 user: "Juan",
-                tipo: "viajero",
+                tipo: "otro",
                 date: "14/2/2020",
                 time: "11:58",
                 text: "Es bastante desierta si, pero Chapita se la banca! Tiene una tracción increible que te permite llegar a donde sea!",
@@ -73,45 +73,7 @@ let data = [{
                 tipo: "otro",
                 date: "14/2/2020",
                 time: "11:58",
-                text: "No se ve bien... pero parece que es a carbon!!",
-            },
-            {
-                image: "https://picsum.photos/id/7/500/",
-                user: "Juan",
-                tipo: "viajero",
-                date: "14/2/2020",
-                time: "11:58",
-                text: "No jaja, es a leña, sino no hubiera dicho asado \"uruguayo\"",
-            }, {
-                image: "https://picsum.photos/id/7/500/",
-                user: "Tío Jaime",
-                tipo: "otro",
-                date: "14/2/2020",
-                time: "11:58",
-                text: "No se ve bien... pero parece que es a carbon!!",
-            },
-            {
-                image: "https://picsum.photos/id/7/500/",
-                user: "Juan",
-                tipo: "viajero",
-                date: "14/2/2020",
-                time: "11:58",
-                text: "No jaja, es a leña, sino no hubiera dicho asado \"uruguayo\"",
-            }, {
-                image: "https://picsum.photos/id/7/500/",
-                user: "Tío Jaime",
-                tipo: "otro",
-                date: "14/2/2020",
-                time: "11:58",
-                text: "No se ve bien... pero parece que es a carbon!!",
-            },
-            {
-                image: "https://picsum.photos/id/7/500/",
-                user: "Juan",
-                tipo: "viajero",
-                date: "14/2/2020",
-                time: "11:58",
-                text: "No jaja, es a leña, sino no hubiera dicho asado \"uruguayo\"",
+                text: "Boquita nomás",
             },
         ],
     },
@@ -187,16 +149,19 @@ function respuesta_generica(imagen, user, date, time, text, tipo) {
             </div>`;
 };
 
-function collapsible(comentarios) {
-    function collapsibleWrapper(comments, length) {
-        return `<ul class="col s12 collapsible hide-on-med-and-up expandable">
-                <li>
-                    <div class="collapsible-header"> <i
-                            class="material-icons">keyboard_arrow_down</i>Comentarios<span>${length}</span>
-                    </div>
-                    <div class="collapsible-body">
-                        ${comments}
-                    </div>
+function collapsible(index, comentarios) {
+    function collapsibleWrapper(idx, comments, length) {
+        return `<ul id="comentarios-collapsible-${idx}" class=" col s12 collapsible hide-on-med-and-up expandable">
+                    <li>
+                        <div class="collapsible-header" style="display: flex; justify-content: space-between;">
+                            <div  style="display: flex; align-content: center;">
+                                <i class="material-icons">keyboard_arrow_down</i><span>Comentarios</span>
+                            </div>
+                            <span class="contador">${length}</span>
+                        </div>
+                        <div class="collapsible-body">
+                            ${comments}
+                        </div>
                     </li>
                 </ul>`;
     };
@@ -211,12 +176,13 @@ function collapsible(comentarios) {
     comentarios.forEach(comentario => {
         bloque_comentarios += wrapper(comentario);
     });
+    bloque_comentarios += inputCollapsible(index);
 
-    let collapsibleHtml = collapsibleWrapper(bloque_comentarios, comentarios.length);
+    let collapsibleHtml = collapsibleWrapper(index, bloque_comentarios, comentarios.length);
     return collapsibleHtml;
 };
 
-function mensajes_grandes(comentarios) {
+function mensajes_grandes(index, comentarios) {
 
     function wrapper(comment) {
         return `<div class="col m9 push-m3 hide-on-small-only">
@@ -229,16 +195,65 @@ function mensajes_grandes(comentarios) {
         bloque_comentarios += wrapper(comentario);
     });
 
+    bloque_comentarios = `<span id="comentarios-grandes-${index}">${bloque_comentarios}</span>`;
+
     return bloque_comentarios;
-}
+};
+
+function inputCollapsible(index) {
+    return `
+    <div class="row ">
+        <div class="row col s12 hide-on-med-and-up ">
+            <div class="input-field inline col s12">
+                <i class="material-icons prefix">account_circle</i>
+                <input id="name-c-${index}"  type="text" class="validate">
+                <label for="name-c-${index}" >¿Quién sos?</label>
+            </div>
+            <div class="input-field inline col s12">
+                <i class="material-icons prefix">short_text</i>
+                <textarea id="textarea-c-${index}"
+                    class="materialize-textarea"></textarea>
+                <label for="textarea-c-${index}">¡Comentá!</label>
+            </div>
+            <div class="col s12" style="display: flex; justify-content: center;">
+                <a id="boton-comentar-c-${index}" class="waves-effect waves-light btn">Comentar</a>
+            </div>
+        </div>
+    </div>
+    `;
+};
+
+function inputGrande(index) {
+    return `
+    <div class="row ">
+        <div class="row col m9 push-m3 hide-on-small-only">
+        <div class="input-field inline col s12">
+                <i class="material-icons prefix">account_circle</i>
+                <input id="name-l-${index}"  type="text" class="validate">
+                <label for="name-l-${index}" >¿Quién sos?</label>
+            </div>
+            <div class="input-field inline col s12">
+                <i class="material-icons prefix">short_text</i>
+                <textarea id="textarea-l-${index}"
+                    class="materialize-textarea"></textarea>
+                <label for="textarea-l-${index}">¡Comentá!</label>
+            </div>
+            <div class="col s12" style="display: flex; justify-content: center;">
+                <a id="boton-comentar-l-${index}" class="waves-effect waves-light btn">Comentar</a>
+            </div>
+        </div>
+    </div>
+    `;
+};
 
 function cargarTweets() {
     let html = "";
 
-    function tweetEntero(principal, grandes, collapsible) {
+    function tweetEntero(principal, grandes, input, collapsible) {
         return `
         ${principal}
         ${grandes}
+        ${input}
         ${collapsible}`;
     };
 
@@ -258,14 +273,103 @@ function cargarTweets() {
             }
             comentariosArray.push(comentarioParseado);
         });
-        grandesHtml = mensajes_grandes(comentariosArray);
-        collapsibleHtml = collapsible(comentariosArray);
-
-        html += tweetEntero(principalHtml, grandesHtml, collapsibleHtml);
+        grandesHtml = mensajes_grandes(index, comentariosArray);
+        collapsibleHtml = collapsible(index, comentariosArray);
+        inputHtml = inputGrande(index);
+        html += tweetEntero(principalHtml, grandesHtml, inputHtml, collapsibleHtml);
     });
 
     document.querySelector("#feed>.section>.row").insertAdjacentHTML('beforeend', html);
-}
+
+    agregarFuncionalidadABotonesDeComentar(data.length);
+};
+
+function agregarFuncionalidadABotonesDeComentar(index) {
+
+    // primero, que al clickear en comentar lea los campos, y si tienen info que agregue un comment
+    for (let i = 0; i < index; i++) {
+        let botonComentarL = document.querySelector(`#boton-comentar-l-${i}`);
+        let botonComentarC = document.querySelector(`#boton-comentar-c-${i}`);
+
+        let campoNameL = document.querySelector(`#name-l-${i}`);
+        let campoNameC = document.querySelector(`#name-c-${i}`);
+
+        let campoCommentL = document.querySelector(`#textarea-l-${i}`);
+        let campoCommentC = document.querySelector(`#textarea-c-${i}`);
+
+
+        // para que los campos de chico y grande estén consistentes
+        campoNameL.addEventListener("input", function () {
+            campoNameC.value = campoNameL.value;
+            M.updateTextFields();
+        });
+        campoNameC.addEventListener("input", function () {
+            campoNameL.value = campoNameC.value;
+            M.updateTextFields();
+        });
+        campoCommentL.addEventListener("input", function () {
+            campoCommentC.value = campoCommentL.value;
+            M.updateTextFields();
+        });
+        campoCommentC.addEventListener("input", function () {
+            campoCommentL.value = campoCommentC.value;
+            M.updateTextFields();
+        });
+
+        // función que le voy a poner a ambos botones
+        function ponerComentario() {
+            let nombre = campoNameL.value;
+            let texto = campoCommentL.value;
+            if (!nombre || !texto) {
+                return;
+            }
+
+            let date = new Date();
+            let y = date.getFullYear();
+            let M = date.getMonth() + 1;
+            let d = date.getDate();
+            let fecha = `${d}/${M}/${y}`;
+            let h = date.getHours().toString().padStart(2, "0");
+            let m = date.getMinutes().toString().padStart(2, "0");
+            let hora = `${h}:${m}`;
+            // crear snippet
+            // naranja o gris
+            let tipo = nombre === data[i].user ? "viajero" : "otro";
+            let comentarioHtml_SIN_WRAPPEAR = (tipo === "viajero") ?
+                respuesta_viajero("https://picsum.photos/id/7/500/", nombre, fecha, hora, texto) :
+                respuesta_generica("https://picsum.photos/id/7/500/", nombre, fecha, hora, texto);
+
+            let comentarioHtmlL = `<div class="col m9 push-m3 hide-on-small-only">
+                                            ${comentarioHtml_SIN_WRAPPEAR}
+                                        </div>`;
+            let comentarioHtmlC = `<div class="hide-on-med-and-up">
+                                        ${comentarioHtml_SIN_WRAPPEAR}
+                                    </div>`;
+
+            // agrego el comment a cada lista correspondiente
+            document.querySelector(`#comentarios-grandes-${i}`).insertAdjacentHTML('beforeend', comentarioHtmlL);
+            document.querySelector(`#comentarios-collapsible-${i}>li>.collapsible-body>.row`).insertAdjacentHTML('beforebegin', comentarioHtmlC);
+
+            let counter = document.querySelector(`#comentarios-collapsible-${i}>li>.collapsible-header>.contador`);
+            counter.innerHTML = parseInt(counter.innerHTML) + 1;
+
+
+            // vacío los campos
+            campoNameL.value = "";
+            campoCommentL.value = "";
+            campoNameC.value = "";
+            campoCommentC.value = "";
+            M.updateTextFields();
+        };
+
+        botonComentarL.addEventListener("click", ponerComentario);
+        botonComentarC.addEventListener("click", ponerComentario);
+    };
+
+
+
+};
+
 
 
 
@@ -308,43 +412,41 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // cuando cliqueo en Full Story, le pongo la altura a tabs de fullstory sections
     document.querySelector('a[href^="#full-story"]').addEventListener('click', function () {
-        clearInterval();
         secciones = document.querySelector("#full-story>div");
         newHeight = secciones.clientHeight;
         document.querySelector('div.tabs-content.carousel.carousel-slider').style.height = newHeight + 'px';
-        arreglarProblemaDeTamanio();
     });
     // cuando cliqueo en Feed, le pongo la altura a tabs de feed
     document.querySelector('a[href^="#feed"]').addEventListener('click', function () {
-        clearInterval();
         secciones = document.querySelector("#feed>div.section");
         newHeight = secciones.clientHeight;
         document.querySelector('div.tabs-content.carousel.carousel-slider').style.height = newHeight + 'px';
-        arreglarProblemaDeTamanio();
     });
 
-    arreglarProblemaDeTamanio();
+    // le seteo el tamaño actualizado a los comments cuando hago un click adentro, que capaz es en los comments
+    document.querySelector("#feed>div.section").addEventListener('click', function () {
+        setTimeout(function () {
+            secciones = document.querySelector("#feed>div.section");
+            newHeight = secciones.clientHeight;
+            document.querySelector('div.tabs-content.carousel.carousel-slider').style.height = newHeight + 'px';
+        }, 250);
+    });
+    // lo mismo con el essay
+    document.querySelector("#full-story>div").addEventListener('click', function () {
+        secciones = document.querySelector("#full-story>div");
+        newHeight = secciones.clientHeight;
+        document.querySelector('div.tabs-content.carousel.carousel-slider').style.height = newHeight + 'px';
+    });
 
-
+    window.onresize = function () {
+        if(document.querySelector('a[href^="#feed"]').classList.contains("active")){
+            document.querySelector("#feed>div.section").click();
+        } else {
+            document.querySelector("#full-story>div");
+        }
+    };
 
 });
-
-function arreglarProblemaDeTamanio() {
-    setInterval(function () {
-        let feed = document.querySelector('a[href^="#feed"]');
-        let fullStory = document.querySelector('a[href^="#full-story"]');
-
-        if (feed.classList.contains("active")) {
-            this.setTimeout(function () {
-                feed.click();
-            }, 250);
-        } else if (fullStory.classList.contains("active")) {
-            this.setTimeout(function () {
-                fullStory.click();
-            }, 250);
-        }
-    }, 2000);
-};
 
 function inicializarBotonesDeReaccion() {
 
